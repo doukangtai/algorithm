3229b767098b4b7b96dffa61f065a9b0634f023e
doukangtai <2636693728@qq.com>
2020-10-07 15:29:07 +0800

    update

--- src/learn/huffmancode/HuffmanCode.java
+++ src/learn/huffmancode/HuffmanCode.java
@@ -22,21 +22,30 @@ public class HuffmanCode {
      */
     public static Integer lastLen;
 
-//    public static void main(String[] args) {
-////        String content = "i like like like java do you like a java";
-//        String content = "i like";
-//        byte[] contentBytes = content.getBytes();
-//        byte[] huffmanCodeBytes = huffmanZip(contentBytes);
-//
-//        byte[] decode = decode(huffmanCodeBytes);
-//        System.out.println(new String(decode));
-//    }
-
     public static void main(String[] args) {
-//        zipFile("C:\\Users\\DKT\\Desktop\\setu.jpg", "F:\\setuFile\\setu.huffman");
-        unZipFile("F:\\setuFile\\setu.huffman", "F:\\setuFile\\decodeSetuFile\\setu.jpg");
+//        String content = "i like like like java do you like a java";
+        String content = "i like";
+        byte[] contentBytes = content.getBytes();
+        byte[] huffmanCodeBytes = huffmanZip(contentBytes);
+
+        byte[] decode = decode(huffmanCodeBytes);
+        System.out.println(new String(decode));
     }
 
+//    public static void main(String[] args) {
+//        zipFile("C:\\Users\\DKT\\Desktop\\setu.jpg", "F:\\setuFile\\setu.huffman");
+//        unZipFile("F:\\setuFile\\setu.huffman", "F:\\setuFile\\decodeSetuFile\\setu.jpg");
+//    }
+
+    /**
+     * 解压缩文件
+     * 通过对象输入流将huffmanZip字节数组、huffmanCodes哈夫曼表、lastLen二进制字符串最后一组长度
+     * 三个对象获取到，
+     * 借助decode方法获取解压缩后的字节数组decode，
+     * 再通过文件输出流将获取到的decode字节数组输出，即得到解压缩文件
+     * @param srcFile
+     * @param dstFile
+     */
     public static void unZipFile(String srcFile, String dstFile) {
         ObjectInputStream objectInputStream = null;
         FileOutputStream fileOutputStream = null;
@@ -64,6 +73,15 @@ public class HuffmanCode {
         }
     }
 
+    /**
+     * 压缩文件
+     * 通过文件输入流将文件读入bytes数组中，
+     * 再通过huffmanZip方法将bytes压缩，
+     * 然后将压缩后的huffmanZip（压缩后的字节数组）、huffmanCodes（哈夫曼表）、lastLen（二进制最后一组长度）
+     * 借助objectOutputStream写出到指定文件
+     * @param srcFile
+     * @param dstFile
+     */
     public static void zipFile(String srcFile, String dstFile) {
         FileInputStream fileInputStream = null;
         ObjectOutputStream objectOutputStream = null;
@@ -90,6 +108,14 @@ public class HuffmanCode {
         }
     }
 
+    /**
+     * 将压缩过的byte[]进行解压缩
+     * 首先借助byteToBitString获取最初的二进制字符串，
+     * 将huffmanCodes哈夫曼表反转，
+     * 通过与reverseMap反转的哈夫曼表对比，获取原始的字节数组，并返回
+     * @param huffmanCodeBytes
+     * @return
+     */
     public static byte[] decode(byte[] huffmanCodeBytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < huffmanCodeBytes.length; i++) {
@@ -123,6 +149,16 @@ public class HuffmanCode {
         return bytes;
     }
 
+    /**
+     * 传入一个byte转化成二进制对应的string，
+     * 注意是否需要补位，
+     * 需要补位：temp |= 256;256即100000000，按位与进行补位，最后注意截取最后8位；
+     * 不需要补位：如果是负数，则需要截取后8位，不是负数，根据lastLen判断是否需要在前面补0，
+     * 若需要则补，否则返回结果
+     * @param b
+     * @param flag
+     * @return
+     */
     public static String byteToBitString(byte b, boolean flag) {
         int temp = b;
         if (flag) {
