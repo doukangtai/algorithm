44ff97a23610371455cab5e96c96bdeab1835411
doukangtai <2636693728@qq.com>
2020-10-25 21:58:08 +0800

    update

---
+++ src/operatingsystemalgorithm/themeone/processsynchronizationmodel/ProcessSynchronizationModel.java
@@ -0,0 +1,8 @@
+package operatingsystemalgorithm.themeone.processsynchronizationmodel;
+
+/**
+ * @author 窦康泰
+ * @date 2020/10/25
+ */
+public class ProcessSynchronizationModel {
+}
---
+++ src/operatingsystemalgorithm/themethree/bankeralgorithm/BankerAlgorithm.java
@@ -0,0 +1,216 @@
+package operatingsystemalgorithm.themethree.bankeralgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author 窦康泰
+ * @date 2020/10/25
+ */
+public class BankerAlgorithm {
+
+    public static int[] resource;
+    public static Process[] processes;
+    public static int processCount = 5;
+    public static int resourceCount = 3;
+    public static int pointer = 0;
+    public static List<Process> processList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        init();
+        initNeed();
+        bankerAlgorithm();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        showFirstTable();
+        showSecondTable();
+    }
+
+    public static void showSecondTable() {
+        System.out.println("Process        Work            Need         Allocation   Work+Allocation   Finish");
+        System.out.println("\t\t\tA   B   C\t\tA   B   C\t\tA   B   C\t\tA   B   C");
+        for (int i = 0; i < processList.size(); i++) {
+            Process process = processList.get(i);
+            System.out.print("  " + process.name + "\t\t");
+            for (int j = 0; j < process.work.length; j++) {
+                System.out.printf("%-4d", process.work[j]);
+            }
+            System.out.print("\t");
+            for (int j = 0; j < process.need.length; j++) {
+                System.out.printf("%-4d", process.need[j]);
+            }
+            System.out.print("\t");
+            for (int j = 0; j < process.allocation.length; j++) {
+                System.out.printf("%-4d", process.allocation[j]);
+            }
+            System.out.print("\t");
+            for (int j = 0; j < resourceCount; j++) {
+                System.out.printf("%-4d", process.work[j] + process.allocation[j]);
+            }
+            System.out.println("\ttrue");
+        }
+        System.out.println("Therefore,To is a (safe) state");
+        System.out.print("Safe sequence is {");
+        for (int i = 0; i < processList.size(); i++) {
+            if (i == 0) {
+                System.out.print(processList.get(i).name);
+            } else {
+                System.out.print("," + processList.get(i).name);
+            }
+        }
+        System.out.println("}");
+    }
+
+    public static void showFirstTable() {
+        System.out.println("Process    Max          Allocation        Need          Available");
+        System.out.println("\t\tA   B   C\t\tA   B   C\t\tA   B   C\t\tA   B   C");
+        for (int i = 0; i < processes.length; i++) {
+            System.out.print(processes[i].name + "\t\t");
+            for (int j = 0; j < resourceCount; j++) {
+                System.out.printf("%-4d", processes[i].max[j]);
+            }
+            System.out.print("\t");
+            for (int j = 0; j < resourceCount; j++) {
+                System.out.printf("%-4d", processes[i].allocation[j]);
+            }
+            System.out.print("\t");
+            for (int j = 0; j < resourceCount; j++) {
+                System.out.printf("%-4d", processes[i].need[j]);
+            }
+            if (i == 0) {
+                System.out.print("\t");
+                int[] originalAvailable = getOriginalAvailable();
+                for (int j = 0; j < originalAvailable.length; j++) {
+                    System.out.printf("%-4d", originalAvailable[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void bankerAlgorithm() {
+        int[] tempAvailable = getOriginalAvailable();
+        for (int i = 0; i < processCount; i++) {
+            Process isExeProcess = getIsExeProcess(tempAvailable);
+            pointerMove();
+            processList.add(isExeProcess);
+            for (int j = 0; j < tempAvailable.length; j++) {
+                tempAvailable[j] = isExeProcess.work[j] + isExeProcess.allocation[j];
+            }
+        }
+    }
+
+    public static int[] getOriginalAvailable() {
+        int[] tempAvailable = new int[resourceCount];
+        for (int i = 0; i < resourceCount; i++) {
+            int tempSum = 0;
+            for (int j = 0; j < processes.length; j++) {
+                tempSum += processes[j].allocation[i];
+            }
+            tempAvailable[i] = resource[i] - tempSum;
+        }
+        return tempAvailable;
+    }
+
+    public static Process getIsExeProcess(int[] tempAvailable) {
+        int breakCount = 0;
+        while (true) {
+            int tempCount = 0;
+            for (int j = 0; j < processes[pointer].need.length; j++) {
+                if (processes[pointer].need[j] <= tempAvailable[j]) {
+                    tempCount++;
+                }
+            }
+            breakCount++;
+            if (tempCount == 3 && !processes[pointer].isUsed) {
+                for (int i = 0; i < processes[pointer].work.length; i++) {
+                    processes[pointer].work[i] = tempAvailable[i];
+                }
+                processes[pointer].isUsed = true;
+                return processes[pointer];
+            } else if (breakCount >= processCount) {
+                break;
+            }
+            pointerMove();
+        }
+        return null;
+    }
+
+    public static void pointerMove() {
+        if (pointer != processCount - 1) {
+            pointer++;
+        } else {
+            pointer = 0;
+        }
+    }
+
+    public static void initNeed() {
+        for (int i = 0; i < processes.length; i++) {
+            int tempCount = 3;
+            for (int j = 0; j < tempCount; j++) {
+                processes[i].need[j] = processes[i].max[j] - processes[i].allocation[j];
+            }
+        }
+    }
+
+    public static void init() {
+        System.out.print("请输入全部资源总量（空格分隔）：");
+        Scanner scanner = new Scanner(System.in);
+        resource = new int[resourceCount];
+        for (int i = 0; i < resourceCount; i++) {
+            resource[i] = scanner.nextInt();
+        }
+        processes = new Process[processCount];
+        for (int i = 0; i < processCount; i++) {
+            System.out.print("请输入P" + i + "的Max和Allocation（空格分隔）：");
+            int[] max = new int[resourceCount];
+            int[] allocation = new int[resourceCount];
+            int[] need = new int[resourceCount];
+            int[] work = new int[resourceCount];
+            for (int j = 0; j < max.length; j++) {
+                max[j] = scanner.nextInt();
+            }
+            for (int j = 0; j < allocation.length; j++) {
+                allocation[j] = scanner.nextInt();
+            }
+            processes[i] = new Process("P" + i, max, allocation, need, work);
+        }
+    }
+
+}
+
+class Process {
+    public String name;
+    public int[] max;
+    public int[] allocation;
+    public int[] need;
+    public int[] work;
+    public boolean isUsed;
+
+    public Process(String name, int[] max, int[] allocation, int[] need, int[] work) {
+        this.name = name;
+        this.max = max;
+        this.allocation = allocation;
+        this.need = need;
+        this.work = work;
+    }
+
+    @Override
+    public String toString() {
+        return "Process{" +
+                "\nname='" + name + '\'' +
+                ", \nmax=" + Arrays.toString(max) +
+                ", \nallocation=" + Arrays.toString(allocation) +
+                ", \nneed=" + Arrays.toString(need) +
+                ", \nwork=" + Arrays.toString(work) +
+                ", \nisUsed=" + isUsed +
+                "}\n\n";
+    }
+}
