98cc576ecbe52a28267fc861df1e20e20986e243
doukangtai <2636693728@qq.com>
2021-01-20 22:08:29 +0800

    jdk动态代理

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * @Author 窦康泰
  * @Date 2020-08-21 10:14
@@ -7,61 +11,48 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        Pizza testPz = new Pizza();
-        testPz.setStatus(Pizza.PizzaStatus.READY);
-        System.out.println(testPz.isDeliverable());
+        MyInterface myProxyObject = (MyInterface) MyProxy.getMyProxyObject(new MyClass());
+        String say = myProxyObject.say("你好啊，小伙子");
+        System.out.println(say);
     }
 }
 
-class Pizza {
+class MyProxy {
+    public static Object getMyProxyObject(Object o) {
+        Object newProxyInstance = Proxy.newProxyInstance(
+                o.getClass().getClassLoader(),
+                o.getClass().getInterfaces(),
+                new MyInvocationHandler(o)
+        );
+        return newProxyInstance;
+    }
+}
 
-    private PizzaStatus status;
-    public enum PizzaStatus {
-        ORDERED (5){
-            @Override
-            public boolean isOrdered() {
-                return true;
-            }
-        },
-        READY (2),
-        DELIVERED (0){
-            @Override
-            public boolean isDelivered() {
-                return true;
-            }
-        };
+class MyInvocationHandler implements InvocationHandler {
 
-        private int timeToDelivery;
+    private Object object;
 
-        public boolean isOrdered() {return false;}
-
-        public boolean isReady() {return false;}
-
-        public boolean isDelivered(){return false;}
-
-        public int getTimeToDelivery() {
-            return timeToDelivery;
-        }
-
-        PizzaStatus (int timeToDelivery) {
-            this.timeToDelivery = timeToDelivery;
-        }
+    public MyInvocationHandler(Object object) {
+        this.object = object;
     }
 
-    public boolean isDeliverable() {
-        return this.status.isReady();
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("before");
+        Object invoke = method.invoke(object, args);
+        System.out.println("after");
+        return invoke;
     }
+}
 
-    public void printTimeToDeliver() {
-        System.out.println("Time to delivery is " +
-                this.getStatus().getTimeToDelivery());
+class MyClass implements MyInterface {
+    @Override
+    public String say(String msg) {
+        System.out.println("说的内容：" + msg);
+        return msg;
     }
+}
 
-    public PizzaStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(PizzaStatus status) {
-        this.status = status;
-    }
+interface MyInterface {
+    String say(String msg);
 }
