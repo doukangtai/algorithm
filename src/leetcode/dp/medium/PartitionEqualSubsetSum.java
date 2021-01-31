43630e6822c94786b37e4b170238d139d4863e7c
doukangtai <2636693728@qq.com>
2021-01-31 10:14:56 +0800

    子集背包

---
+++ src/leetcode/dp/medium/PartitionEqualSubsetSum.java
@@ -0,0 +1,38 @@
+package leetcode.dp.medium;
+
+/**
+ * @author 窦康泰
+ * @date 2021/01/31
+ */
+public class PartitionEqualSubsetSum {
+    public static void main(String[] args) {
+        System.out.println(new PartitionEqualSubsetSum()
+                .canPartition(new int[]{1, 5, 11, 5}));
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum /= 2;
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (j - nums[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,65 +1,15 @@
 package test;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
-import java.util.concurrent.ForkJoinTask;
-import java.util.concurrent.RecursiveTask;
-
 /**
  * @author 窦康泰
  * @date 2021/01/21
  */
 public class Test {
     public static void main(String[] args) {
-        ThreadLocal<Integer> threadLocal1 = new ThreadLocal<>();
-        ThreadLocal<Integer> threadLocal2 = new ThreadLocal<>();
-        new Thread(() -> {
-            threadLocal1.set(111);
-            threadLocal2.set(222);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("AAA " + threadLocal1.get());
-            System.out.println("AAA " + threadLocal2.get());
-        }).start();
-        new Thread(() -> {
-            threadLocal1.set(1);
-            threadLocal2.set(2);
-            System.out.println("BBB " + threadLocal1.get());
-            System.out.println("BBB " + threadLocal2.get());
-        }).start();
-        threadLocal1.remove();
-        threadLocal2.remove();
-    }
-}
-
-class ForkJoinExample extends RecursiveTask<Integer> {
-    private int threshold = 5;
-    private int first;
-    private int last;
-
-    public ForkJoinExample(int first, int last) {
-        this.first = first;
-        this.last = last;
-    }
-
-    @Override
-    protected Integer compute() {
-        if (last - first <= threshold) {
-            int sum = 0;
-            for (int i = first; i <= last; i++) {
-                sum += i;
-            }
-            return sum;
-        } else {
-            int mid = first + (last - first) / 2;
-            ForkJoinExample forkJoinExample1 = new ForkJoinExample(first, mid);
-            ForkJoinExample forkJoinExample2 = new ForkJoinExample(mid + 1, last);
-            ForkJoinTask<Integer> fork1 = forkJoinExample1.fork();
-            ForkJoinTask<Integer> fork2 = forkJoinExample2.fork();
-            return forkJoinExample1.join() + forkJoinExample2.join();
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
