eb150b4bbb9944643cfa622319a3137090538526
doukangtai <2636693728@qq.com>
2021-02-01 17:29:37 +0800

    反转m到n链表

---
+++ src/leetcode/linkedlist/medium/ReverseLinkedListii.java
@@ -0,0 +1,61 @@
+package leetcode.linkedlist.medium;
+
+/**
+ * @author 窦康泰
+ * @date 2021/02/01
+ */
+public class ReverseLinkedListii {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        ListNode node2 = root.next = new ListNode(2);
+        node2.next = new ListNode(3);
+        System.out.println(root);
+        System.out.println(new ReverseLinkedListii().reverseBetween(root, 2, 3));
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+
+    private ListNode succNext;
+
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            succNext = head.next;
+            return head;
+        }
+        ListNode lase = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = succNext;
+        return lase;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,15 +1,31 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author 窦康泰
  * @date 2021/01/21
  */
 public class Test {
     public static void main(String[] args) {
-        try {
-            Thread.sleep(1000000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        List<Person> list = new ArrayList<>();
+        while (true) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(new Person(new Random().nextInt(1024 * 1024)));
         }
+    }
+}
+
+class Person {
+    byte[] bytes;
+
+    public Person(int i) {
+        this.bytes = new byte[i];
     }
 }
