f50f35cd8ee83a1382b4874c39d64ef798426b2b
doukangtai <2636693728@qq.com>
2021-03-05 21:54:37 +0800

    回溯

---
+++ src/leetcode/dp/hard/EditDistance.java
@@ -0,0 +1,41 @@
+package leetcode.dp.hard;
+
+/**
+ * @author 窦康泰
+ * @date 2021/03/05
+ */
+public class EditDistance {
+    public static void main(String[] args) {
+        System.out.println(new EditDistance().minDistance("horse", "ros"));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(
+                            dp[i - 1][j],
+                            dp[i][j - 1],
+                            dp[i - 1][j - 1]
+                    ) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    private int min(int num1, int num2, int num3) {
+        return Math.min(num1, Math.min(num2, num3));
+    }
+}
