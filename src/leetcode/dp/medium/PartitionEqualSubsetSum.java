a6b8ae8cddb9af48f0b6c448dedccb1b23d04875
doukangtai <2636693728@qq.com>
2021-04-01 19:53:21 +0800

    DP

--- src/leetcode/dp/medium/PartitionEqualSubsetSum.java
+++ src/leetcode/dp/medium/PartitionEqualSubsetSum.java
@@ -35,4 +35,35 @@ public class PartitionEqualSubsetSum {
         }
         return dp[n][sum];
     }
+
+    static class Method2 {
+        public static void main(String[] args) {
+            System.out.println(new Method2().canPartition(new int[]{1, 2, 5}));
+        }
+
+        public boolean canPartition(int[] nums) {
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            if (sum % 2 == 1) {
+                return false;
+            }
+            sum /= 2;
+            boolean[][] dp = new boolean[nums.length + 1][sum + 1];
+            for (int i = 0; i < nums.length + 1; i++) {
+                dp[i][0] = true;
+            }
+            for (int i = 1; i < nums.length + 1; i++) {
+                for (int j = 0; j < sum + 1; j++) {
+                    if (j - nums[i - 1] >= 0) {
+                        dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[nums.length][sum];
+        }
+    }
 }
--- src/leetcode/dp/medium/TargetSum.java
+++ src/leetcode/dp/medium/TargetSum.java
@@ -26,4 +26,37 @@ public class TargetSum {
         help(nums, S, index + 1);
         S -= nums[index];
     }
+
+    static class Method2 {
+        public static void main(String[] args) {
+            System.out.println(new Method2().findTargetSumWays(new int[]{0,0,0,0,0,0,0,0,1}, 1));
+        }
+
+        public int findTargetSumWays(int[] nums, int S) {
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            if (sum < S) {
+                return 0;
+            }
+            sum = sum + S;
+            if (sum % 2 != 0) {
+                return 0;
+            }
+            sum /= 2;
+            int[][] dp = new int[nums.length + 1][sum + 1];
+            dp[0][0] = 1;
+            for (int i = 1; i < nums.length + 1; i++) {
+                for (int j = 0; j < sum + 1; j++) {
+                    if (j - nums[i - 1] >= 0) {
+                        dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[nums.length][sum];
+        }
+    }
 }
